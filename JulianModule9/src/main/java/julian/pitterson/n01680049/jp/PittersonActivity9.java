8581893ff992529ce681016b2ef3f0b7e97ef859
Grimgrams <63823807+Grimgrams@users.noreply.github.com>
2026-03-24 22:38:19 -0400

    implemented splash screen

--- JulianModule9/src/main/java/julian/pitterson/n01680049/jp/PittersonActivity9.java
+++ JulianModule9/src/main/java/julian/pitterson/n01680049/jp/PittersonActivity9.java
@@ -6,6 +6,7 @@ import android.os.Bundle;
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
+import androidx.core.splashscreen.SplashScreen;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
@@ -13,6 +14,8 @@ public class PittersonActivity9 extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        SplashScreen splashScreen = SplashScreen.installSplashScreen(this);
+
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
@@ -21,5 +24,10 @@ public class PittersonActivity9 extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        long startTime = System.currentTimeMillis();
+        splashScreen.setKeepOnScreenCondition(() ->
+                System.currentTimeMillis() - startTime < 3000
+        );
     }
 }
